272266bf369b6b1f3621be567d5973eacc2f8be5
Eric Peterson <ericpeterson91@live.ca>
2022-02-17 16:59:02 -0600

    yo

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 
 
-from .models import Photo, Profile
+from .models import  Profile
 
 # Register your models here.
 
-admin.site.register(Photo)
+# admin.site.register(Photo)
 from .models import  Profile
 
 # Register your models here.
