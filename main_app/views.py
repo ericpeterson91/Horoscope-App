5807412f041c2c89443ff72ac9a9871e2a1a9268
Eric Peterson <ericpeterson91@live.ca>
2022-02-17 23:30:27 -0600

    herllo

--- main_app/views.py
+++ main_app/views.py
@@ -209,25 +209,6 @@ class ProfileDelete(DeleteView):
   model = Profile
   success_url = '/profile/new'
 
-@login_required
-def add_photo(request):
-    # photo-file will be the "name" attribute on the <input type="file">
-    photo_file = request.FILES.get('photo-file', None)
-    if photo_file:
-        s3 = boto3.client('s3')
-        # need a unique "key" for S3 / needs image file extension too
-        key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
-        # just in case something goes wrong
-        try:
-            s3.upload_fileobj(photo_file, BUCKET, key)
-            # build the full url string
-            url = f"{S3_BASE_URL}{BUCKET}/{key}"
-            # we can assign to cat_id or cat (if you have a cat object)
-            photo = Photo(url=url)
-            photo.save()
-        except:
-            print('An error occurred uploading file to S3')
-    return redirect('/profile/')
 
 @login_required
 def horoscope(request):
