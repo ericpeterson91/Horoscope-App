052f8e592e8de30f1924eb30383edb7260957712
Eric Peterson <ericpeterson91@live.ca>
2022-02-17 23:25:13 -0600

    yolo

--- main_app/views.py
+++ main_app/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .models import Horoscope, Photo, Profile
+from .models import Horoscope, Profile
 from django.contrib.auth.models import User
 from django.db.models import Q
 
@@ -94,85 +94,85 @@ def matches(request):
         matches = User.objects.filter(
             Q(horoscope__horoscope = 'CP') | Q(horoscope__horoscope = 'CA') | Q(horoscope__horoscope = 'TA') | Q(horoscope__horoscope = 'SC'))[:3]
         print(matches)
-        # sign_text = request.match.horoscope.horoscope
-        # if sign_text =='CP':
-        #     request.match.horoscope.text = 'Capricorn'
-        # if sign_text =='CA':
-        #     request.match.horoscope.text = 'Cancer' 
-        # if sign_text =='TA':
-        #     request.match.horoscope.text = 'Taurus'
-        # if sign_text =='SC':
-        #     request.match.horoscope.text = 'Scorpio'
+        sign_text = request.match.horoscope.horoscope
+        if sign_text =='CP':
+            request.match.horoscope.text = 'Capricorn'
+        if sign_text =='CA':
+            request.match.horoscope.text = 'Cancer' 
+        if sign_text =='TA':
+            request.match.horoscope.text = 'Taurus'
+        if sign_text =='SC':
+            request.match.horoscope.text = 'Scorpio'
         return render(request, 'matches.html', { 'matches': matches, 'user': request.user })
     if user_sign == 'AR' or user_sign == 'LI':
         matches = User.objects.filter(
             Q(horoscope__horoscope = 'GE') | Q(horoscope__horoscope = 'LE') | Q(horoscope__horoscope = 'SA') | Q(horoscope__horoscope = 'AQ'))[:3]
         print(matches)        
-        # sign_text = request.match.horoscope.horoscope
-        # if sign_text =='GE':
-        #     request.match.horoscope.text = 'Gemini'
-        # if sign_text =='LE':
-        #     request.match.horoscope.text = 'Leo' 
-        # if sign_text =='SA':
-        #     request.match.horoscope.text = 'Sagittarius'
-        # if sign_text =='AQ':
-        #     request.match.horoscope.text = 'Aquarius'
+        sign_text = request.match.horoscope.horoscope
+        if sign_text =='GE':
+            request.match.horoscope.text = 'Gemini'
+        if sign_text =='LE':
+            request.match.horoscope.text = 'Leo' 
+        if sign_text =='SA':
+            request.match.horoscope.text = 'Sagittarius'
+        if sign_text =='AQ':
+            request.match.horoscope.text = 'Aquarius'
         return render(request, 'matches.html', { 'matches': matches, 'user': request.user })
     if user_sign == 'TA' or user_sign == 'SC':
         matches = User.objects.filter(
             Q(horoscope__horoscope = 'CA') | Q(horoscope__horoscope = 'VI') | Q(horoscope__horoscope = 'CP') | Q(horoscope__horoscope = 'PI'))[:3]
         print(matches)         
-        # sign_text = request.match.horoscope.horoscope
-        # if sign_text =='CP':
-        #     request.match.horoscope.text = 'Capricorn'
-        # if sign_text =='CA':
-        #     request.match.horoscope.text = 'Cancer' 
-        # if sign_text =='PI':
-        #     request.match.horoscope.text = 'Pisces'
-        # if sign_text =='VI':
-        #     request.match.horoscope.text = 'Virgo'
+        sign_text = request.match.horoscope.horoscope
+        if sign_text =='CP':
+            request.match.horoscope.text = 'Capricorn'
+        if sign_text =='CA':
+            request.match.horoscope.text = 'Cancer' 
+        if sign_text =='PI':
+            request.match.horoscope.text = 'Pisces'
+        if sign_text =='VI':
+            request.match.horoscope.text = 'Virgo'
         return render(request, 'matches.html', { 'matches': matches, 'user': request.user })
     if user_sign == 'GE' or user_sign == 'SA':
         matches = User.objects.filter(
             Q(horoscope__horoscope = 'LI') | Q(horoscope__horoscope = 'AQ') | Q(horoscope__horoscope = 'AR') | Q(horoscope__horoscope = 'LE'))[:3]
         print(matches)     
-        # sign_text = request.match.horoscope.horoscope
-        # if sign_text =='LI':
-        #     request.match.horoscope.text = 'Libra'
-        # if sign_text =='AQ':
-        #     request.match.horoscope.text = 'Aquarius' 
-        # if sign_text =='AR':
-        #     request.match.horoscope.text = 'Aries'
-        # if sign_text =='LE':
-        #     request.match.horoscope.text = 'Leo'
+        sign_text = request.match.horoscope.horoscope
+        if sign_text =='LI':
+            request.match.horoscope.text = 'Libra'
+        if sign_text =='AQ':
+            request.match.horoscope.text = 'Aquarius' 
+        if sign_text =='AR':
+            request.match.horoscope.text = 'Aries'
+        if sign_text =='LE':
+            request.match.horoscope.text = 'Leo'
         return render(request, 'matches.html', { 'matches': matches, 'user': request.user })
     if user_sign == 'CA' or user_sign == 'CP':
         matches = User.objects.filter(
             Q(horoscope__horoscope = 'TA') | Q(horoscope__horoscope = 'VI') | Q(horoscope__horoscope = 'SC') | Q(horoscope__horoscope = 'PI'))[:3]
         print(matches)   
-        # sign_text = request.match.horoscope.horoscope
-        # if sign_text =='TA':
-        #     request.match.horoscope.text = 'Taurus'
-        # if sign_text =='VI':
-        #     request.match.horoscope.text = 'Virgo' 
-        # if sign_text =='PI':
-        #     request.match.horoscope.text = 'Pisces'
-        # if sign_text =='SC':
-        #     request.match.horoscope.text = 'Scorpio'
+        sign_text = request.match.horoscope.horoscope
+        if sign_text =='TA':
+            request.match.horoscope.text = 'Taurus'
+        if sign_text =='VI':
+            request.match.horoscope.text = 'Virgo' 
+        if sign_text =='PI':
+            request.match.horoscope.text = 'Pisces'
+        if sign_text =='SC':
+            request.match.horoscope.text = 'Scorpio'
         return render(request, 'matches.html', { 'matches': matches, 'user': request.user })
     if user_sign == 'LE' or user_sign == 'AQ':
         matches = User.objects.filter(
             Q(horoscope__horoscope = 'AR') | Q(horoscope__horoscope = 'GE') | Q(horoscope__horoscope = 'LI') | Q(horoscope__horoscope = 'SA'))[:3]
         print(matches)       
-        # sign_text = request.match.horoscope.horoscope
-        # if sign_text =='AR':
-        #     request.match.horoscope.text = 'Aries'
-        # if sign_text =='GE':
-        #     request.match.horoscope.text = 'Gemini' 
-        # if sign_text =='LI':
-        #     request.match.horoscope.text = 'Libra'
-        # if sign_text =='SA':
-        #     request.match.horoscope.text = 'Sagittarius'
+        sign_text = request.match.horoscope.horoscope
+        if sign_text =='AR':
+            request.match.horoscope.text = 'Aries'
+        if sign_text =='GE':
+            request.match.horoscope.text = 'Gemini' 
+        if sign_text =='LI':
+            request.match.horoscope.text = 'Libra'
+        if sign_text =='SA':
+            request.match.horoscope.text = 'Sagittarius'
         return render(request, 'matches.html', { 'matches': matches, 'user': request.user })
 
 class ProfileCreate(LoginRequiredMixin, CreateView):
@@ -207,25 +207,6 @@ class ProfileDelete(DeleteView):
   model = Profile
   success_url = '/profile/new'
 
-@login_required
-def add_photo(request):
-    # photo-file will be the "name" attribute on the <input type="file">
-    photo_file = request.FILES.get('photo-file', None)
-    if photo_file:
-        s3 = boto3.client('s3')
-        # need a unique "key" for S3 / needs image file extension too
-        key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
-        # just in case something goes wrong
-        try:
-            s3.upload_fileobj(photo_file, BUCKET, key)
-            # build the full url string
-            url = f"{S3_BASE_URL}{BUCKET}/{key}"
-            # we can assign to cat_id or cat (if you have a cat object)
-            photo = Photo(url=url)
-            photo.save()
-        except:
-            print('An error occurred uploading file to S3')
-    return redirect('/profile/')
 
 @login_required
 def horoscope(request):
